005dbc8e3277c190dacb231e4aa47f927d8dc975
LSH <534063714@qq.com>
2017-08-03 14:24:15 +0800

    C# Websocket

---
+++ Server/FrmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Server
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(462, 548);
+            this.listBox1.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 567);
+            this.Controls.Add(this.listBox1);
+            this.Name = "FrmMain";
+            this.Text = "服务器主窗体";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Server/FrmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketEngine;
+using SuperSocket.SocketBase.Logging;
+using SuperWebSocket;
+using log4net;
+namespace Server
+{
+    public partial class FrmMain : Form
+    {
+        WebSocketServer  appServer = new WebSocketServer();
+        ServerConfig  serverConfig = new ServerConfig
+            {
+                Port = 8080,//set the listening port
+                MaxConnectionNumber = 10000
+            };
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {        
+          if (!appServer.Setup(serverConfig)) //Setup the appServer
+            {
+                System.Windows.Forms.MessageBox.Show("开启服务器失败");
+                return;
+            }
+
+            if (!appServer.Start())//Try to start the appServer
+            {
+                System.Windows.Forms.MessageBox.Show("开启服务器失败");
+                return;
+            }
+            //注册事件
+            appServer.NewSessionConnected += appServer_NewSessionConnected;//客户端连接
+            appServer.NewMessageReceived += appServer_NewMessageReceived;//客户端接收消息
+            appServer.SessionClosed += appServer_SessionClosed;//客户端关闭
+        }
+        void appServer_NewSessionConnected(WebSocketSession session)
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;  //屏蔽线程间空间操作报错问题，也可以选择委托方法解决
+
+            session.Send("连接成功");
+            listBox1.Items.Add("Host:"+session.Host);//服务器的ip
+            listBox1.Items.Add("Uri:"+session.UriScheme);
+            listBox1.Items.Add("Path:"+session.Path);
+            listBox1.Items.Add("CurrentToken:" + session.CurrentToken);
+            listBox1.Items.Add("SessionID:" + session.SessionID);
+            listBox1.Items.Add("Connection" + session.Connection);
+            listBox1.Items.Add("Origin"+session.Origin);
+            listBox1.Items.Add("LocalEndPoint"+session.LocalEndPoint);
+            listBox1.Items.Add("RemoteEndPoint" + session.RemoteEndPoint);
+            
+        }
+        void appServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            session.Send("服务端收到了客户端发来的消息");
+            this.listBox1.Items.Add(value);
+
+        }
+        void appServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
+        {
+            this.listBox1.Items.Add(value.ToString());
+            session.Close();
+        }
+    }
+}
